fa418e4854c3e5d539ad90c9bce3bb9feb34f3c0
DKD07 <161266358+DKD07@users.noreply.github.com>
2024-04-03 07:10:33 +0530

    Add files via upload

---
+++ Recursive fact.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    
+    if(n==0 or n==1):
+       return 1
+    else:
+        return n * factorial(n-1)
+x=int(input("Enter the number: "))
+output=factorial(x)
+print("The output is : ",output )
